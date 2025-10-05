7a61a21c4676ad3c9671b7a95639e2070c9e4cf0
ledetav <nord.hilman@gmail.com>
2025-10-05 11:34:24 +0300

    feat: replace 'add artifact' button with icon button

--- app/src/main/java/com/nokaori/genshinaibuilder/ui/ArtifactScreen.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/ui/ArtifactScreen.kt
@@ -1,17 +1,11 @@
 package com.nokaori.genshinaibuilder.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -19,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.Alignment
 import com.nokaori.genshinaibuilder.data.Artifact
 import com.nokaori.genshinaibuilder.data.ArtifactStat
 import com.nokaori.genshinaibuilder.data.StatValue
@@ -30,12 +25,23 @@ fun ArtifactScreen(artifactViewModel: ArtifactViewModel = viewModel()) {
     val searchedArtifacts by artifactViewModel.searchedArtifacts.collectAsState()
 
     Column(modifier = Modifier.padding(24.dp)) {
-        Text(
-            text = "Артефакты",
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Артефакты",
+                style = MaterialTheme.typography.headlineMedium
+            )
 
+            IconButton(onClick = { artifactViewModel.addDefaultaArtifact() }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Добавить артефакт"
+                )
+            }
+        }
         OutlinedTextField(
             value = searchQuery,
             onValueChange = { newText ->
@@ -45,20 +51,6 @@ fun ArtifactScreen(artifactViewModel: ArtifactViewModel = viewModel()) {
             label = { Text("Поиск по артефактам") },
             singleLine = true
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                artifactViewModel.addDefaultaArtifact()
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = "Добавить артефакт",
-                style = MaterialTheme.typography.titleMedium
-            )
-        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
