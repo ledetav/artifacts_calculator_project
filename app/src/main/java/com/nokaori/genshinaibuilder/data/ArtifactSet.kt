e2fd1629097b2c933573cdec0dc972de819ad529
ledetav <nord.hilman@gmail.com>
2025-10-06 10:01:45 +0300

    feat: add initial ArtifactSet data class

---
+++ app/src/main/java/com/nokaori/genshinaibuilder/data/ArtifactSet.kt
@@ -0,0 +1,10 @@
+package com.nokaori.genshinaibuilder.data
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Style
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class ArtifactSet(
+    val name: String,
+    val icon: ImageVector = Icons.Default.Style // Заглушка
+)
