9642ce8c0c1a5cbbaa534bde7a8ad663338536f2
ledetav <nord.hilman@gmail.com>
2025-10-20 09:25:39 +0300

    feat: Create AppDrawer and MainTopAppBar components

---
+++ app/src/main/java/com/nokaori/genshinaibuilder/ui/common/components/AppBars.kt
@@ -0,0 +1,42 @@
+package com.nokaori.genshinaibuilder.ui.common.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api)
+@Composable
+fun MainToAppBar(
+    title: String,
+    onNavigationIconClick: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Открыть навигационное меню"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    )
+}
