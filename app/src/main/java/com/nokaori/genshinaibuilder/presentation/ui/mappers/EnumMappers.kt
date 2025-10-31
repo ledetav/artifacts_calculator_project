134d787a7a0791f8259593ef0f13cfc0d2e86a85
ledetav <nord.hilman@gmail.com>
2025-11-01 01:41:02 +0300

    feat: Create mappers to get display names for enums

---
+++ app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/mappers/EnumMappers.kt
@@ -0,0 +1,45 @@
+package com.nokaori.genshinaibuilder.presentation.ui.mappers
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.nokaori.genshinaibuilder.R
+import com.nokaori.genshinaibuilder.domain.model.ArtifactSlot
+import com.nokaori.genshinaibuilder.domain.model.StatType
+
+@Composable
+fun ArtifactSlot.toDisplayName(): String {
+    return when (this) {
+        ArtifactSlot.FLOWER_OF_LIFE -> stringResource(R.string.artifact_slot_flower)
+        ArtifactSlot.PLUME_OF_DEATH -> stringResource(R.string.artifact_slot_plume)
+        ArtifactSlot.SANDS_OF_EON -> stringResource(R.string.artifact_slot_sands)
+        ArtifactSlot.GOBLET_OF_EONOTHEM -> stringResource(R.string.artifact_slot_goblet)
+        ArtifactSlot.CIRCLET_OF_LOGOS -> stringResource(R.string.artifact_slot_circlet)
+    }
+}
+
+@Composable
+fun StatType.toDisplayName(): String {
+    val stringId = when (this) {
+        StatType.ATK -> R.string.stat_type_atk
+        StatType.DEF -> R.string.stat_type_def
+        StatType.HP -> R.string.stat_type_hp
+        StatType.ATK_PERCENT -> R.string.stat_type_atk_percent
+        StatType.DEF_PERCENT -> R.string.stat_type_def_percent
+        StatType.HP_PERCENT -> R.string.stat_type_hp_percent
+        StatType.CRIT_RATE -> R.string.stat_type_crit_rate
+        StatType.CRIT_DMG -> R.string.stat_type_crit_dmg
+        StatType.ENERGY_RECHARGE -> R.string.stat_type_energy_recharge
+        StatType.ELEMENTAL_MASTERY -> R.string.stat_type_elemental_mastery
+        StatType.ANEMO_DAMAGE_BONUS -> R.string.stat_type_anemo_damage_bonus
+        StatType.GEO_DAMAGE_BONUS -> R.string.stat_type_geo_damage_bonus
+        StatType.ELECTRO_DAMAGE_BONUS -> R.string.stat_type_electro_damage_bonus
+        StatType.CRYO_DAMAGE_BONUS -> R.string.stat_type_cryo_damage_bonus
+        StatType.DENDRO_DAMAGE_BONUS -> R.string.stat_type_dendro_damage_bonus
+        StatType.HYDRO_DAMAGE_BONUS -> R.string.stat_type_hydro_damage_bonus
+        StatType.PHYSICAL_DAMAGE_BONUS -> R.string.stat_type_physical_damage_bonus
+        StatType.PYRO_DAMAGE_BONUS -> R.string.stat_type_pyro_damage_bonus
+        StatType.HEALING_BONUS -> R.string.stat_type_healing_bonus
+    }
+
+    return stringResource(stringId)
+}
