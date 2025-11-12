015cb517d52655744ac034b9ee274bbe00444964
ledetav <nord.hilman@gmail.com>
2025-11-12 02:23:32 +0000

    feat: Refactor WeaponTypeFilter to use MultiSelectToggleButtonGroup with title

--- app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/weapons/components/WeaponTypeFilter.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/weapons/components/WeaponTypeFilter.kt
@@ -1,7 +1,10 @@
 package com.nokaori.genshinaibuilder.presentation.ui.weapons.components
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.nokaori.genshinaibuilder.R
 import com.nokaori.genshinaibuilder.domain.model.WeaponType
 import com.nokaori.genshinaibuilder.presentation.ui.common.components.MultiSelectToggleButtonGroup
 import com.nokaori.genshinaibuilder.presentation.ui.mappers.toDisplayName
@@ -13,11 +16,14 @@ fun WeaponTypeFilter(
     modifier: Modifier = Modifier
 ) {
     MultiSelectToggleButtonGroup(
-        options = WeaponType.entries.toSet(),
-        selectedOptions = selectedWeaponTypes,
-        onOptionSelected = onWeaponTypeSelected,
+        title = stringResource(R.string.filter_weapon_type),
+        items = WeaponType.entries.toList(),
+        selectedItems = selectedWeaponTypes,
+        onItemClick = onWeaponTypeSelected,
         modifier = modifier
-    ) { weaponType ->
-        weaponType.toDisplayName()
+    ) { weaponType, isSelected ->
+        Text(
+            text = weaponType.toDisplayName()
+        )
     }
 }
