cf73a8e0a7d3c240fda4303f391a35866ea883ae
Alexander Break <92530287+ledetav@users.noreply.github.com>
2025-12-05 14:32:16 +0000

    refactor: Configure ViewModel dependencies

--- app/src/main/java/com/nokaori/genshinaibuilder/presentation/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/presentation/viewmodel/ViewModelFactory.kt
@@ -2,24 +2,31 @@ package com.nokaori.genshinaibuilder.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.nokaori.genshinaibuilder.data.repository.ArtifactRepositoryImpl
-import com.nokaori.genshinaibuilder.data.repository.WeaponRepositoryImpl
 import com.nokaori.genshinaibuilder.domain.repository.ArtifactRepository
+import com.nokaori.genshinaibuilder.domain.repository.ThemeRepository
 import com.nokaori.genshinaibuilder.domain.repository.WeaponRepository
+import com.nokaori.genshinaibuilder.domain.usecase.FilterArtifactsUseCase
 
 class ViewModelFactory(
     private val artifactRepository: ArtifactRepository,
-    private val weaponRepository: WeaponRepository
+    private val weaponRepository: WeaponRepository,
+    private val themeRepository: ThemeRepository
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(ArtifactViewModel::class.java) -> {
-                ArtifactViewModel(artifactRepository) as T
+                ArtifactViewModel(
+                    artifactRepository = artifactRepository,
+                    filterArtifactsUseCase = FilterArtifactsUseCase()
+                ) as T
             }
             modelClass.isAssignableFrom(WeaponViewModel::class.java) -> {
                 WeaponViewModel(weaponRepository) as T
+            }
+            modelClass.isAssignableFrom(ThemeViewModel::class.java) -> {
+                ThemeViewModel(themeRepository) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
