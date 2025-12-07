628d2a2a61d54b5520b07e7d202140dfdafe3060
Alexander Break <92530287+ledetav@users.noreply.github.com>
2025-12-07 12:14:27 +0000

    refactor: Relocate character models and update dummy data

--- app/src/main/java/com/nokaori/genshinaibuilder/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/data/repository/CharacterRepositoryImpl.kt
@@ -1,15 +1,16 @@
 package com.nokaori.genshinaibuilder.data.repository
 
-import com.nokaori.genshinaibuilder.data.model.Character
-import com.nokaori.genshinaibuilder.data.model.Element
-import com.nokaori.genshinaibuilder.data.model.WeaponType
-import com.nokaori.genshinaibuilder.data.repository.CharacterRepository
+import com.nokaori.genshinaibuilder.domain.model.Character
+import com.nokaori.genshinaibuilder.domain.model.Element
+import com.nokaori.genshinaibuilder.domain.model.WeaponType
+import com.nokaori.genshinaibuilder.domain.repository.CharacterRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class CharacterRepositoryImpl : CharacterRepository {
+
     private val _characters = MutableStateFlow<List<Character>>(emptyList())
 
     init {
@@ -38,75 +39,75 @@ class CharacterRepositoryImpl : CharacterRepository {
         return listOf(
             Character(
                 id = 1,
-                name = "Amber",
+                name = "Hu Tao",
                 element = Element.PYRO,
-                weaponType = WeaponType.SWORD,
+                weaponType = WeaponType.POLEARM,
                 rarity = 5,
-                isOwned = false
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Hutao.png", // Заглушка
+                isOwned = true
             ),
             Character(
                 id = 2,
-                name = "Barbara",
+                name = "Yelan",
                 element = Element.HYDRO,
-                weaponType = WeaponType.CATALYST,
-                rarity = 4,
-                isOwned = false
+                weaponType = WeaponType.BOW,
+                rarity = 5,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Yelan.png",
+                isOwned = true
             ),
             Character(
                 id = 3,
-                name = "Kaeya",
-                element = Element.CRYO,
-                weaponType = WeaponType.SWORD,
-                rarity = 4,
-                isOwned = false
+                name = "Nahida",
+                element = Element.DENDRO,
+                weaponType = WeaponType.CATALYST,
+                rarity = 5,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Nahida.png",
+                isOwned = false // Для теста фильтра "Only Owned"
             ),
             Character(
                 id = 4,
-                name = "Lisa",
+                name = "Raiden Shogun",
                 element = Element.ELECTRO,
-                weaponType = WeaponType.CATALYST,
-                rarity = 4,
-                isOwned = false
+                weaponType = WeaponType.POLEARM,
+                rarity = 5,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Shougun.png",
+                isOwned = true
             ),
             Character(
                 id = 5,
-                name = "Razor",
-                element = Element.ELECTRO,
-                weaponType = WeaponType.CLAYMORE,
-                rarity = 4,
-                isOwned = false
+                name = "Kazuha",
+                element = Element.ANEMO,
+                weaponType = WeaponType.SWORD,
+                rarity = 5,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Kazuha.png",
+                isOwned = true
             ),
-            Character(
+             Character(
                 id = 6,
-                name = "Venti",
-                element = Element.ANEMO,
-                weaponType = WeaponType.BOW,
-                rarity = 5,
-                isOwned = false
-            ),
-            Character(
-                id = 7,
-                name = "Xiao",
-                element = Element.ANEMO,
-                weaponType = WeaponType.POLEARM,
-                rarity = 5,
-                isOwned = false
-            ),
-            Character(
-                id = 8,
-                name = "YunJin",
-                element = Element.GEO,
-                weaponType = WeaponType.POLEARM,
-                rarity = 4,
-                isOwned = false
-            ),
-            Character(
-                id = 9,
                 name = "Zhongli",
                 element = Element.GEO,
                 weaponType = WeaponType.POLEARM,
                 rarity = 5,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Zhongli.png",
                 isOwned = false
+            ),
+            Character(
+                id = 7,
+                name = "Ayaka",
+                element = Element.CRYO,
+                weaponType = WeaponType.SWORD,
+                rarity = 5,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Ayaka.png",
+                isOwned = true
+            ),
+             Character(
+                id = 8,
+                name = "Xiangling",
+                element = Element.PYRO,
+                weaponType = WeaponType.POLEARM,
+                rarity = 4,
+                iconUrl = "https://gi.yatta.moe/assets/UI/UI_AvatarIcon_Xiangling.png",
+                isOwned = true
             )
         )
     }
