f388746b64986b6bf706f95f414e40dd293abda9
Alexander Break <92530287+ledetav@users.noreply.github.com>
2025-12-08 14:40:08 +0000

    feat: Add BuildAlertLevel enum

---
+++ app/src/main/java/com/nokaori/genshinaibuilder/domain/model/BuildAlertLevel.kt
@@ -0,0 +1,7 @@
+package com.nokaori.genshinaibuilder.domain.model
+
+enum class BuildAlertLevel {
+    NONE,    // Всё хорошо (Зеленый)
+    WARNING, // Предупреждение (Желтый)
+    CRITICAL // Критическая ошибка билда (Красный)
+}
