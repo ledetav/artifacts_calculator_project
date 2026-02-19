181aa6b6a722d62fbb11d6e6912a42e3c6412331
ledetav <nord.hilman@gmil.com>
2026-02-19 09:57:47 +0000

    feat(editor): Add showBiometricPrompt to EditorArtifactState

--- app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/artifacts/editor/data/EditorArtifactState.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/artifacts/editor/data/EditorArtifactState.kt
@@ -33,7 +33,7 @@ data class EditorArtifactState(
     val currentMaxTotalRolls: Int = 9,
 
     val validationErrors: List<String> = emptyList(),
-    val isSaveSuccess: Boolean = false
+    val isSaveSuccess: Boolean = false,
     val showBiometricPrompt: Boolean = false,
 )
 
