1adf533796ded2390c7b6eed0accad38ad9829e4
ledetav <nord.hilman@gmil.com>
2026-03-04 11:46:10 +0000

    refactor(deps): update fastdeployOCR package name

--- app/src/main/java/com/nokaori/genshinaibuilder/domain/util/ArtifactTextRecognizer.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/domain/util/ArtifactTextRecognizer.kt
@@ -7,8 +7,8 @@ import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
-import com.equationl.fastdeployOCR.OCR
-import com.equationl.fastdeployOCR.OcrConfig
+import com.equationl.fastdeployocr.OCR
+import com.equationl.fastdeployocr.OcrConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
