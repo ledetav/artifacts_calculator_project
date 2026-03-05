bddac7a731c8fad296faf9d030db020114ccbd80
ledetav <nord.hilman@gmil.com>
2026-03-05 09:26:16 +0000

    refactor: update opencv

--- app/src/main/jni/opencv/sdk/native/jni/include/opencv2/opencv_modules.hpp
+++ app/src/main/jni/opencv/sdk/native/jni/include/opencv2/opencv_modules.hpp
@@ -6,7 +6,12 @@
  *
 */
 
+// This definition means that OpenCV is built with enabled non-free code.
+// For example, patented algorithms for non-profit/non-commercial use only.
+/* #undef OPENCV_ENABLE_NONFREE */
+
 #define HAVE_OPENCV_CORE
+#define HAVE_OPENCV_FEATURES2D
 #define HAVE_OPENCV_HIGHGUI
 #define HAVE_OPENCV_IMGPROC
 #define HAVE_OPENCV_PHOTO
