f376ceb34d4062d255a90f876e78de4ef549167e
ledetav <nord.hilman@gmil.com>
2026-03-12 08:53:20 +0000

    feat(scanner): Add LaunchedEffect for artifact scanning

--- app/src/main/java/com/nokaori/genshinaibuilder/presentation/MainActivity.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/presentation/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
