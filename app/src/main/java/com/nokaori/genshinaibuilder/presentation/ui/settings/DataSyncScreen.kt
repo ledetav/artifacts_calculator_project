9dfcb8594aef50571eff660931432318dc8bc475
ledetav <nord.hilman@gmail.com>
2026-03-29 17:15:44 +0300

    feat(settings): add remember import to DataSyncScreen

--- app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/settings/DataSyncScreen.kt
+++ app/src/main/java/com/nokaori/genshinaibuilder/presentation/ui/settings/DataSyncScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
